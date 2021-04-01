eeacc9a7ea74ce701ffadee0bc9d8db0826b985e
wandayusufalvian <wandayusuf.yusuf@gmail.com>
2021-04-01 08:19:46 +0700

    update

--- Data_Preprocessing/bank_dataset.py
+++ Data_Preprocessing/bank_dataset.py
@@ -1,43 +1,78 @@
+#%%
+'''import libraries'''
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.compose import make_column_transformer 
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.compose import ColumnTransformer
+
 from service import *
 
-'''READ FILES and EXPLORE DATA'''  
-data_bank=pd.read_csv(r"/home/yusuf/hyperparameter-tuning-benchmark/Data_Preprocessing/Dataset/bank-full.csv",sep=';')
-#data_bank.info()
-#print(max(data_bank['age']))
 
-'''CHECK IRRELEVANT VALUE''' 
-show_values(data_bank)
+#%%
+'''read files and explore data'''  
+data_bank=pd.read_csv(r"/home/yusuf/hyperparameter-tuning-benchmark/Data_Preprocessing/dataset-raw/bank-full.csv",sep=';')
 
-'''CALCULATE PERCENTAGE OF 'UNKNOWN' DATA'''
-# print("job : ",288*100/45211)
-# print("education : ",1857*100/45211)
-# print("contact : ",13020*100/45211)
-# print("poutcome : ",36959*100/45211)
+#%%
+'''delete duration column'''
+data_bank=data_bank.drop(['duration'],axis=1)
+# berdasarkan https://archive.ics.uci.edu/ml/datasets/Bank+Marketing, disarankan duration dihapus 
 
-# print(data_bank['contact'])
- 
+#%%
+'''show features'''
+data_bank.info()
 
-'''DELETE UNKNOWN ROWS IN JOB AND EDUCATION COLUMN'''
-data_bank=data_bank.loc[(data_bank.education != 'unknown')]
-data_bank=data_bank.loc[(data_bank.job != 'unknown')]
+#%% 
+'''show categorical values''' 
+# show_values(data_bank)
+
+#%% 
+'''change unknown values => most frequent values'''
+# job = blue-collar 
+# education = secondary 
+# contact = cellular 
+data_bank['job']=data_bank['job'].replace(['unknown'],'blue-collar')
+data_bank['education']=data_bank['education'].replace(['unknown'],'secondary')
+data_bank['contact']=data_bank['contact'].replace(['unknown'],'cellular')
+
+#%%
+'''calculate percentage of unknown data'''
+#print("job : ",288*100/45211)
+#print("education : ",1857*100/45211)
+#print("contact : ",13020*100/45211)
+#print("poutcome : ",36959*100/45211)
+
+#%%
+'''delete poutcome column'''
+data_bank=data_bank.drop(['poutcome'],axis=1)
+
+#%%
+'''categories quantity'''
+#X,y=split_x_y(data_bank)
+#count_categories(X)
+
+#%%
+'''group categories value in job feature'''
+data_bank['job']=data_bank['job'].replace(['management','entrepreneur'],'high')
+data_bank['job']=data_bank['job'].replace(['blue-collar','technician','services','self-employed'],'mid')
+data_bank['job']=data_bank['job'].replace(['unemployed','housemaid','student','admin.','retired'],'low')
+
+'''group categories value in month feature'''
+data_bank['month']=data_bank['month'].replace(['jan','feb','mar','apr','may','jun'],'semester-1')
+data_bank['month']=data_bank['month'].replace(['jul','aug','sep','oct','nov','dec'],'semester-2')
+
 #show_values(data_bank)
 
-'''DELETE POUTCOME COLUMN'''
-data_bank=data_bank.drop(['poutcome'],axis=1)
-#data_bank.info()
-'''DELETE DURATION COLUMN'''
-data_bank=data_bank.drop(['duration'],axis=1)
-# Important note: this attribute highly affects the 
-# output target (e.g., if duration=0 then y='no'). 
-# Yet, the duration is not known before a call is performed.
+#%%
+'''transform and normalize data'''
+X,y=split_x_y(data_bank)
+print("before transform")
+print("y= ",y.shape)
+print("X= ",X.shape)
+X,y=transform_data(X,y)
+print("after transform")
+print("y= ",y.shape)
+print("X= ",X.shape)
 
-'''CHECK OUTLIER IN CONTINUOUS DATA'''
-#plt.boxplot(data_bank['age'])
+#%%
+'''export data'''
+ekspor_data(X,y,"X-bank","y-bank")
--- Data_Preprocessing/service.py
+++ Data_Preprocessing/service.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.compose import ColumnTransformer
 
 def show_values(X):
     cat_df=X.select_dtypes(include='object')
@@ -7,10 +11,31 @@ def show_values(X):
         print(i,'\n')
         print(X[i].value_counts(),'\n')
 
+def split_x_y(data):
+    y=data.iloc[:,-1:]
+    X=data.iloc[:,0:-1]
+    return X,y
+
+def count_categories(X):
+    categorical_ix = X.select_dtypes(include=['object']).columns 
+    sum=0
+    for i in categorical_ix:
+        print(i,"=",len(X[i].unique())," categories")
+        sum=sum+len(X[i].unique())
+    print("\n total categories= ",sum)
+
+def transform_data(X,y):
+    y=y.to_numpy().ravel()
+    y=LabelEncoder().fit_transform(y)
+    numerical_ix = X.select_dtypes(include=['int64']).columns
+    categorical_ix = X.select_dtypes(include=['object']).columns
+    t = [('cat', OneHotEncoder(sparse=False), categorical_ix), ('num', MinMaxScaler(), numerical_ix)]
+    col_transform = ColumnTransformer(transformers=t)
+    X=col_transform.fit_transform(X)
+    return X,y
 
 def ekspor_data(X,y,nama_X,nama_y):
     X=pd.DataFrame(X)
     y=pd.DataFrame(y)
-    X.to_csv('dataset/'+nama_X+'.csv',index=False)
-    y.to_csv('dataset/'+nama_y+'.csv',index=False)
-
+    X.to_csv('dataset-ready/'+nama_X+'.csv',index=False,header=None)
+    y.to_csv('dataset-ready/'+nama_y+'.csv',index=False,header=None)
