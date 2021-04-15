f09b6c7427c685b8d6a3dc9953521efa08f19d33
wandayusufalvian <wandayusuf.yusuf@gmail.com>
2021-04-15 10:24:02 +0700

    update

---
+++ Experiments/experiment_1_2.py
@@ -0,0 +1,12 @@
+import services.data as data
+import services.gbdt as gbdt
+import services.optimize as opt 
+
+iterasi=1
+
+for i in range(0,iterasi):
+    X,y=data.baca_data_bank()
+    model=gbdt.xgboost_model()
+    hasil=opt.default_hyperparameter(X,y,model)
+    print("Iterasi ke-"+str(i+1)+"\n")
+    data.simpan_hasil_hpc(hasil)
---
+++ Experiments/experiment_1_3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --job-name=experiment_1_3
+#SBATCH --output=experiment_1_3.txt
+#
+#SBATCH --time=30:00
+#SBATCH --nodes=1
+#SBATCH --nodelist=komputasi06
+
+source /home/m448735/anaconda3/bin/activate
+srun python experiment_1_2.py
