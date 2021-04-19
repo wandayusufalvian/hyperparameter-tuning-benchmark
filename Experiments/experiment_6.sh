aee2cdba9b3c9a1c55203a117876240dcf9e0841
wandayusufalvian <wandayusuf.yusuf@gmail.com>
2021-04-19 11:59:36 +0700

    update

---
+++ Experiments/experiment_6.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --job-name=experiment_6
+#SBATCH --output=results/experiment_6_lightgbm_bank_default.txt
+#
+#SBATCH --time=11:59:00
+#SBATCH --nodes=1
+#SBATCH --nodelist=komputasi06
+
+source /home/m448735/anaconda3/bin/activate
+srun python experiment_6_lightgbm_bank_default.py
