b88854d5031ac0c9f0fe1bba314037d18ea1f658
wandayusufalvian <wandayusuf.yusuf@gmail.com>
2021-04-19 10:06:05 +0700

    update

--- Experiments/experiment_5.sh
+++ Experiments/experiment_5.sh
@@ -5,7 +5,7 @@
 #
 #SBATCH --time=11:59:00
 #SBATCH --nodes=1
-#SBATCH --nodelist=komputasi04
+#SBATCH --nodelist=komputasi06
 
 source /home/m448735/anaconda3/bin/activate
 srun python experiment_5_xgboost_bank_bohb.py
