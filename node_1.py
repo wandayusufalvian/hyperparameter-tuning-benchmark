1802aa6ea3e9f34a0b0e995d501aa526d6bea17a
wandayusufalvian <wandayusuf.yusuf@gmail.com>
2021-06-21 22:57:56 +0700

    update

--- node_1.py
+++ node_1.py
@@ -4,28 +4,42 @@ import services.models as gbdt
 import services.hyperparameter as hyper
 
 
-print("experiment 27 : catboost + dataset 2 + grid search"+"\n")
-
-experiments.exp_grid_search(
-                            dataset=reader_writer.baca_data_cus_seg_catboost(),
-                            mesin=0,
-                            file_name="--",
-                            model=gbdt.catboost_model_2(),
-                            hyperparameter=hyper.hyper_catboost_gs(),
-                            eval_method="accuracy")
-
-print("\n")
-
-print("experiment 28 : catboost + dataset 2 + random search"+"\n")
+print("experiment 33 : xgboost + dataset 3 + random search"+"\n")
 
 experiments.exp_random_search(
-                            dataset=reader_writer.baca_data_cus_seg_catboost(),
+                            dataset=reader_writer.baca_data_housing(),
                             mesin=0,
                             file_name="--",
-                            model=gbdt.catboost_model_2(),
-                            hyperparameter=hyper.hyper_catboost_rs(),
+                            model=gbdt.xgboost_model_reg(),
+                            hyperparameter=hyper.hyper_xgboost_rs(),
                             seeds=[1,12,22,32,42,52,62,72,82,92],
-                            eval_method="accuracy")
+                            eval_method="neg_mean_absolute_error")
 
 print("\n")
 
+print("experiment 34 : xgboost + dataset 3 + bayes search"+"\n")
+
+experiments.exp_bayes_search(
+                            dataset=reader_writer.baca_data_housing(),
+                            mesin=0,
+                            file_name="--",
+                            model=gbdt.xgboost_model_reg(),
+                            hyperparameter=hyper.hyper_xgboost_bo(),
+                            seeds=[1,12,22,32,42,52,62,72,82,92],
+                            eval_method="neg_mean_absolute_error")
+
+print("\n")
+
+# 43
+print("experiment 43 : catboost + dataset 3 + random search"+"\n")
+
+experiments.exp_random_search(
+                            dataset=reader_writer.baca_data_housing_catboost(),
+                            mesin=0,
+                            file_name="--",
+                            model=gbdt.catboost_model_2_reg(),
+                            hyperparameter=hyper.hyper_catboost_rs(),
+                            seeds=[1,12,22,32,42,52,62,72,82,92],
+                            eval_method="neg_mean_absolute_error")
+
+print("\n")
--- node_2.py
+++ node_2.py
@@ -3,18 +3,6 @@ import services.reader_writer as reader_writer
 import services.models as gbdt
 import services.hyperparameter as hyper
 
-print("experiment 29 : catboost + dataset 2 + bayes search"+"\n")
-
-experiments.exp_bayes_search(
-                            dataset=reader_writer.baca_data_cus_seg_catboost(),
-                            mesin=0,
-                            file_name="--",
-                            model=gbdt.catboost_model_2(),
-                            hyperparameter=hyper.hyper_catboost_bo(),
-                            seeds=[1,12,22,32,42,52,62,72,82,92],
-                            eval_method="accuracy")
-
-print("\n")
 
 
 print("experiment 31 : xgboost + dataset 3 + default hyperparameter"+"\n")
@@ -25,7 +13,7 @@ experiments.exp_default(
                         file_name="--",
                         iterasi=10,
                         model=gbdt.xgboost_model_reg(),
-                        eval_method="mae")
+                        eval_method="neg_mean_absolute_error")
 
 print("\n")
 
@@ -37,7 +25,7 @@ experiments.exp_default(
                         file_name="--",
                         iterasi=10,
                         model=gbdt.lightgbm_model_reg(),
-                        eval_method="mae")
+                        eval_method="neg_mean_absolute_error")
 
 print("\n")
 
@@ -49,7 +37,7 @@ experiments.exp_default(
                         file_name="--",
                         iterasi=10,
                         model=gbdt.catboost_model_2_reg(),
-                        eval_method="mae")
+                        eval_method="neg_mean_absolute_error")
 
 print("\n")
 
@@ -61,7 +49,7 @@ experiments.exp_grid_search(
                             file_name="--",
                             model=gbdt.xgboost_model_reg(),
                             hyperparameter=hyper.hyper_xgboost_gs(),
-                            eval_method="mae")
+                            eval_method="neg_mean_absolute_error")
 
 print("\n")
 
@@ -73,7 +61,7 @@ experiments.exp_grid_search(
                             file_name="--",
                             model=gbdt.lightgbm_model_reg(),
                             hyperparameter=hyper.hyper_lightgbm_gs(),
-                            eval_method="mae")
+                            eval_method="neg_mean_absolute_error")
 
 print("\n")
 
@@ -85,5 +73,48 @@ experiments.exp_grid_search(
                             file_name="--",
                             model=gbdt.catboost_model_2_reg(),
                             hyperparameter=hyper.hyper_catboost_gs(),
-                            eval_method="mae")
+                            eval_method="neg_mean_absolute_error")
+print("\n")
+
+
+# 38, 39 
+
+print("experiment 38 : lightgbm + dataset 3 + random search"+"\n")
+
+experiments.exp_random_search(
+                            dataset=reader_writer.baca_data_housing(),
+                            mesin=0,
+                            file_name="--",
+                            model=gbdt.lightgbm_model_reg(),
+                            hyperparameter=hyper.hyper_lightgbm_rs(),
+                            seeds=[1,12,22,32,42,52,62,72,82,92],
+                            eval_method="neg_mean_absolute_error")
+
+print("\n")
+
+print("experiment 39 : lightgbm + dataset 3 + bayes search"+"\n")
+
+experiments.exp_bayes_search(
+                            dataset=reader_writer.baca_data_housing(),
+                            mesin=0,
+                            file_name="--",
+                            model=gbdt.lightgbm_model_reg(),
+                            hyperparameter=hyper.hyper_lightgbm_bo(),
+                            seeds=[1,12,22,32,42,52,62,72,82,92],
+                            eval_method="neg_mean_absolute_error")
+
+print("\n")
+
+# 44
+print("experiment 44 : catboost + dataset 3 + bayes search"+"\n")
+
+experiments.exp_bayes_search(
+                            dataset=reader_writer.baca_data_housing_catboost(),
+                            mesin=0,
+                            file_name="--",
+                            model=gbdt.catboost_model_2_reg(),
+                            hyperparameter=hyper.hyper_catboost_bo(),
+                            seeds=[1,12,22,32,42,52,62,72,82,92],
+                            eval_method="neg_mean_absolute_error")
+
 print("\n")
